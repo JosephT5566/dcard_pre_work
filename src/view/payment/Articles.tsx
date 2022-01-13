f6ab68b9994a47a29ed0a0e4171fd304590a8b43
Your JosephTseng <zxp930110@hotmail.com.tw>
2022-01-13 16:26:33 +0800

    install moment and style the post

--- src/view/payment/Articles.tsx
+++ src/view/payment/Articles.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import moment from 'moment'
 
 import { styled } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
@@ -17,10 +18,32 @@ const ArticlesContainer = styled('div')({
 
 const StyledPost = styled('div')({
     padding: '2em 1em',
-    display: 'flex',
-    flexDirection: 'column',
+    display: 'grid',
+    gridTemplateColumns: '1fr auto',
+    gridTemplateRows: 'auto auto auto auto',
+    gridTemplateAreas: `
+    "id id"
+    "header header"
+    "title thumb"
+    "main thumb"
+    `,
     gap: '1em',
     borderBottom: 'solid 1px gray',
+    '& .id': {
+        gridArea: 'id',
+    },
+    '& .header': {
+        gridArea: 'header',
+    },
+    '& .title': {
+        gridArea: 'title',
+    },
+    '& .main': {
+        gridArea: 'main',
+    },
+    '& .thumb': {
+        gridArea: 'thumb',
+    },
 })
 
 export default function Articles() {
@@ -37,7 +60,7 @@ export default function Articles() {
         window.addEventListener('scroll', scrollEvent)
 
         return () => window.removeEventListener('scroll', scrollEvent)
-    }, [])
+    }, [refetch])
 
     return (
         <ArticlesContainer>
@@ -66,15 +89,48 @@ const Post = (props: { post: IPost }) => {
 
     return (
         <StyledPost>
-            <Typography variant={'subtitle1'} sx={{ fontSize: '0.5rem' }}>
+            <Typography
+                className={'id'}
+                variant={'subtitle1'}
+                sx={{ fontSize: '0.5rem' }}
+            >
                 {post.id}
             </Typography>
-            <Typography variant={'h2'} sx={{ fontSize: '1.5rem', fontWeight: 'bold' }}>
+            <div className={'header'} style={{ display: 'flex', gap: '1em' }}>
+                <Typography variant={'subtitle1'} sx={{ fontSize: '0.5rem' }}>
+                    {post.forumName}
+                </Typography>
+                <Typography variant={'subtitle1'} sx={{ fontSize: '0.5rem' }}>
+                    {moment(post.createAt).format('YYYY-MM-DD')}
+                </Typography>
+            </div>
+            <Typography
+                className={'title'}
+                variant={'h2'}
+                sx={{ fontSize: '1.5rem', fontWeight: 'bold' }}
+            >
                 {post.title}
             </Typography>
-            <Typography variant={'body1'} sx={{ color: 'gray' }}>
+            <Typography className={'main'} variant={'body1'} sx={{ color: 'gray' }}>
                 {post.excerpt}
             </Typography>
+            {post.media.length > 0 ? (
+                <div
+                    className={'thumb'}
+                    style={{
+                        width: 84,
+                        height: 84,
+                        overflow: 'hidden',
+                        borderRadius: '12px',
+                    }}
+                >
+                    <img
+                        src={post.media[0].url}
+                        alt="media"
+                        style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                    />
+                </div>
+            ) : null}
         </StyledPost>
     )
 }
